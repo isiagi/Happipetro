93e6a3c8683ac80ce3accb96a34067797cc3c5d1
isiagi <isiagigeofrey0@gmail.com>
2022-06-21 10:06:37 +0300

    adds banner

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,22 @@
+import "./banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner__container">
+      <div className="banner__one">
+        <h1>The biggest supplier in the country</h1>
+      </div>
+      <div className="banner__two">
+        <p>
+          Happi Petro is a registered company situated in Uganda dealing in Oil
+          and Gas Exploration, mining, extraction, refining tranpostation,
+          distribution and marketing of petroleum / oil products, with a
+          clientele of over 15 clients and a aim of serving 1 million liters and
+          above per month.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import Hero1 from '../components/hero1/Hero1'
-import Nav from '../components/nav/Nav'
+import Banner from "../components/banner/Banner";
+import Hero1 from "../components/hero1/Hero1";
+import Nav from "../components/nav/Nav";
 
 const HomePage = () => {
   return (
     <div>
-        <Nav />
-        <Hero1 />
+      <Nav />
+      <Hero1 />
+      <Banner />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
