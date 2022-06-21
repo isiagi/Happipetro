68891309d56175948022cdc5bcd6d7999e426961
isiagi <isiagigeofrey0@gmail.com>
2022-06-21 13:01:13 +0300

    adds map

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import Hero1 from "../components/hero1/Hero1";
 import Img from "../components/imgM/Img";
 import Join from "../components/join/Join";
 import Learn from "../components/learn/Learn";
+import MapComponent from "../components/map/map";
 import Nav from "../components/nav/Nav";
 import Service from "../components/services/Service";
 
@@ -16,6 +17,7 @@ const HomePage = () => {
       <Img />
       <Service />
       <Learn />
+      <MapComponent />
       <Join />
       <Footer />
     </div>
