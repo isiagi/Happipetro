6a8f57aaa3ed364d8e064b685d20df2b3e87451f
isiagi <isiagigeofrey0@gmail.com>
2022-06-21 08:37:19 +0300

    initial app, add nav

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,22 @@
+import "./nav.css";
+
+const Nav = () => {
+  return (
+    <div className="nav__container">
+      <div className="nav__wrapper">
+        <h1>HappiPetro</h1>
+        <div className="nav__ul">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Service</li>
+            <li>Gallery</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
