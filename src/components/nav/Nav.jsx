eebae6272c05021b52f322e679573ab423bc15d7
isiagi <isiagigeofrey0@gmail.com>
2022-06-22 04:13:39 +0300

    adds routing in nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,17 +1,31 @@
+import { Link } from "react-router-dom";
+
 import "./nav.css";
 
 const Nav = () => {
   return (
     <div className="nav__container">
       <div className="nav__wrapper">
-        <h1>HappiPetro</h1>
+        <h1>
+          <Link to="/">HappiPetro</Link>
+        </h1>
         <div className="nav__ul">
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Service</li>
-            <li>Gallery</li>
-            <li className="nav__contact">Contact</li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/service">Service</Link>
+            </li>
+            <li>
+              <Link to="/gallery">Gallery</Link>
+            </li>
+            <li className="nav__contact">
+              <Link to="/contact">Contact</Link>
+            </li>
           </ul>
         </div>
       </div>
