fa0bdac9d2d6a285ed32086c53ae7d67024480cf
isiagi <isiagigeofrey0@gmail.com>
2022-06-22 03:15:04 +0300

    adds contact and footer to router

---
+++ src/pages/contact/Contact.jsx
@@ -0,0 +1,46 @@
+import MapComponent from "../../components/map/map";
+import {MdOutlineMarkEmailRead} from 'react-icons/md'
+import {BsFillTelephoneOutboundFill} from 'react-icons/bs'
+import {HiOutlineLocationMarker} from 'react-icons/hi'
+import "./contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact__container">
+      <div className="contact__head">
+        <h1>Contact</h1>
+      </div>
+      <div className="contact__grid">
+        <div className="contact__sec1">
+          <h2>Contact us to get more information</h2>
+          <p><MdOutlineMarkEmailRead className="contact__icon" /> ffsgsyysus@gmail.com</p>
+          <p><BsFillTelephoneOutboundFill className="contact__icon"/> +256777797887777</p>
+          <p><HiOutlineLocationMarker className="contact__icon"/> Ham Towers</p>
+        </div>
+        <div className="contact__sec2">
+          <form>
+            <div className="form__row">
+              <input type="text" placeholder="First Name" />
+              <input type="text" placeholder="Last Name" />
+            </div>
+            <div className="form__row">
+              <input type="text" placeholder="Enter Email" />
+              <input type="text" placeholder="Enter Company" />
+            </div>
+            <div className="form__row textarea">
+              <textarea rows={9} placeholder="Enter Massage Here" />
+            </div>
+            <div className="form__btn">
+              <button type="submit" className="btn">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <MapComponent />
+    </div>
+  );
+};
+
+export default Contact;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "../components/nav/Nav";
+import Footer from "../components/footer/Footer";
 import Contact from "./contact/Contact";
 import HomePage from "./HomePage";
 
 const index = () => {
   return (
     <Router>
-        <Nav />
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/contact" element={<Contact />} />
-        </Routes>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </Router>
-  )
-}
+  );
+};
 
-export default index
+export default index;
